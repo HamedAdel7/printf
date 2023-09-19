165327e98e0fb84a140b3cca930943d1d835aa9d
HamedAdel7 <hamedadel2021@gmail.com>
2023-09-19 12:30:06 -0700

    1

---
+++ _put.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _puts - print syring
+ * @str: string print
+ * Return: void
+*/
+
+int _puts(char *str)
+{
+	char *a = str;
+
+	while (*str)
+		_putchar(*str++);
+	return (str - a);
+}
+
+/**
+ * _putchar - print char
+ * @c: the char
+ * Return: 1 success
+*/
+
+int _putchar(int c)
+{
+	static int i;
+	static char buf[OUTOUT_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
